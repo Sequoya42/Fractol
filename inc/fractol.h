98133aed988464155d1f026c06a20afb9da55416
Raphael BAUM <rbaum@e1r10p14.42.fr>
2015-09-09 16:40:42 +0200

    regular commit

--- inc/fractol.h
+++ inc/fractol.h
@@ -6,7 +6,7 @@
 /*   By: rbaum <rbaum@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/08 19:34:29 by rbaum             #+#    #+#             */
-/*   Updated: 2015/09/09 16:08:21 by rbaum            ###   ########.fr       */
+/*   Updated: 2015/09/09 16:34:45 by rbaum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,24 @@
 
 #include "../libft/libft.h"
 #include <mlx.h>
+#include <math.h>
+
+# define WIN_X 800
+# define WIN_Y 800
+# define MAX_ITER 256
+
+# define CXMIN -2.5
+# define CXMAX -1.5
+# define CYMIN -2.0
+# define CYMAX 2.0
+
+# define PIXW (CXMAX - CXMIN) / WIN_X
+# define PIXH (CYMAX - CYMIN) / WIN_Y
+
+typedef struct  s_env
+{
+    void        *mlx;
+    void        *win;
+}               t_env;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,15 +6,49 @@
 /*   By: rbaum <rbaum@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/08 19:34:00 by rbaum             #+#    #+#             */
-/*   Updated: 2015/09/09 16:09:27 by rbaum            ###   ########.fr       */
+/*   Updated: 2015/09/09 16:40:31 by rbaum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+#include <stdio.h>
+#include <math.h>
 
-int		main(void)
-{
-	mlx_init();
-	sleep(2);
-	return 0;
+int lerp(float v0, float v1, float t) {
+	return (1 - t ) * v0 + t * v1;
 }
+
+float normalize(double x, double y, int i)
+{
+	double zn = sqrt(x * x + y * y);
+	double nu = log(log(zn) / log(2)) / log(2);
+	return (i + 1 - nu);
+}
+
+int rainbow_color(int i, double x, double y)
+{
+	float frequency = .8;
+
+	if (i == MAX_ITER)
+		return (0);
+	int k = floor(normalize(x, y, i));
+	int red = sin(frequency * k) * 127 + 128;
+	int green = sin(frequency * k + 2) * 127 + 128;
+	int blue = sin(frequency * k + 4) * 127 + 128;
+	int red_2 = sin(frequency * (k + 1)) * 127 + 128;
+	int green_2 = sin(frequency * (k + 1) + 2) * 127 + 128;
+	int blue_2 = sin(frequency * (k + 1) + 4) * 127 + 128;
+	return ((lerp(red, red_2, i % 1) << 16) +
+			(lerp(blue, blue_2, i % 1) << 8) + lerp(green, green_2, i % 1));
+}
+
+int main(void)
+{
+	t_env e;
+
+	e.mlx = mlx_init();
+	e.win = mlx_new_window(e.mlx, WIN_X, WIN_Y, "Fractol");
+	mlx_loop(e.mlx);
+	return (0);
+}
+
